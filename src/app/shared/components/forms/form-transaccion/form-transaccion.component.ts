71f7007f597a508fbbf270b495240aea3d44255b
Miguel Ruiz Zamora <58411768+miguelruiz7@users.noreply.github.com>
2024-02-11 19:41:34 -0600

    11/02/2024

--- src/app/shared/components/forms/form-transaccion/form-transaccion.component.ts
+++ src/app/shared/components/forms/form-transaccion/form-transaccion.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FirebaseService } from 'src/app/services/firebase.service';
+import { UtilsService } from 'src/app/services/utils.service';
 
 @Component({
   selector: 'app-form-transaccion',
@@ -7,8 +10,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormTransaccionComponent  implements OnInit {
 
+  utilsSvc =  inject(UtilsService);
+  firebaseSvc = inject(FirebaseService);
+
+
+  form = new FormGroup({
+    trans_tipo_id: new FormControl(''),
+    trans_cant:  new FormControl(null,  [Validators.required, Validators.pattern(/^\d+(\.\d{1,2})?$/)]),
+    trans_desc: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    trans_cat_ing:  new FormControl(''),
+    trans_cta_id: new FormControl(''),
+    trans_metodo_cobro: new FormControl(''),
+    trans_cat_per: new FormControl(''),
+    
+  });
+
+
+
+
+  trans_tipos : Array<{ trans_tipo_id: string, trans_tipo_nom: string }> = [
+    { trans_tipo_id: "ingreso", trans_tipo_nom: 'Ingreso' },
+    { trans_tipo_id: "transaccion", trans_tipo_nom: 'Transaccion' },
+  ];
+
+
+  metodos_tipos : Array<{ metodo_tipo_id: string, metodo_tipo_nom: string }> = [
+    { metodo_tipo_id: "contado", metodo_tipo_nom: 'Contado' },
+    { metodo_tipo_id: "credito", metodo_tipo_nom: 'Crédito' },
+  ];
+
+
+
   constructor() { }
 
+
   ngOnInit() {}
+
+
+  cerrarModal(){
+    this.utilsSvc.cancelarModal();
+ }
+
+
+  enviar(){
+
+  }
 
 }
